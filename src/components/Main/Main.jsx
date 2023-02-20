7eba68cd98a8e329b4ff3558eb035309ab11423e
Ignacio Kruchowski <ignakruchowski@gmail.com>
2023-02-20 12:29:10 -0300

    Agregar helper items con info

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,10 @@
 import "./Main.css";
+import {contentList} from "../../helper/helper"
 
 export const Main = () => {
   return (
     <div>
+      
     </div>
   )
 }
---
+++ src/helper/helper.js
@@ -0,0 +1,69 @@
+import Video1 from "../assets/videos/keyframes.mp4 (1080p).mp4";
+import Video2 from "../assets/videos/manual_de_pedidos_del_hincha(1080p).mp4";
+import Video3 from "../assets/videos/modo_federal(1080p).mp4";
+import Video5 from "../assets/videos/Refresh_Galicia_©2023.mp4";
+import Video6 from "../assets/videos/tumblr_ronnksgdvo1zljdcp_r1_720(540p).mp4";
+import Video7 from "../assets/videos/verano_galicia(1080p).mp4";
+
+export const contentList = [
+    {
+        id: 0,
+        name: "Refresh Galicia",
+        description: null,
+        copyright: null,
+        by: null,
+        content: Video5,
+        tagName: "refresh-galicia",
+        type: "horizontal"
+    }, 
+    {
+        id: 1,
+        name: "Keyframes",
+        description: "Norman Wiseman (Podcast)",
+        copyright: "&#169; 2022",
+        by: "Personal Project",
+        content: Video1,
+        tagName: "keyframes-project",
+        type: "vertical"
+    },
+    {
+        id: 2,
+        name: "Verano Galicia",
+        description: null,
+        copyright: "&#169; 2022",
+        by: "ft. Wunderman Thompson",
+        content: Video7,
+        tagName: "verano-galicia",
+        type: "horizontal"
+    },
+    {
+        id: 3,
+        name: "Manual de Pedidos del Hincha",
+        description: "Campaña Mundial Qatar 2022",
+        copyright: "&#169; 2022",
+        by: "ft. Lanzallamas TV for PedidosYa",
+        content: Video2,
+        tagName: "manual-pedidosya",
+        type: "vertical"
+    },
+    {
+        id: 4,
+        name: "Modo Federal",
+        description: "Social Media",
+        copyright: "&#169; 2022",
+        by: "ft. Lanzallamas TV for MODO",
+        content: Video3,
+        tagName: "modo-federal",
+        type: "vertical"
+    },
+    {
+        id: 5,
+        name: "Comisión O",
+        description: "Social Media",
+        copyright: "&#169; 2022",
+        by: "ft. Lanzallamas TV for ENIGMA",
+        content: Video6,
+        tagName: "comisión-enigma",
+        type: "vertical"
+    }
+]
