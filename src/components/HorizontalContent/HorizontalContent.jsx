61f0abee2877e91062e402ef3d1415b3e16f3f1a
Ignacio Kruchowski <ignakruchowski@gmail.com>
2023-02-27 20:32:46 -0300

    Agregar margen InfoContainer y controlador de muted en Main

--- src/components/HorizontalContent/HorizontalContent.jsx
+++ src/components/HorizontalContent/HorizontalContent.jsx
@@ -1,9 +1,52 @@
 import "./HorizontalContent.css";
+import { BsFillVolumeMuteFill } from "react-icons/bs";
+import { BsFillVolumeUpFill } from "react-icons/bs";
+import { useState } from "react";
 
 export const HorizontalContent = ({item}) => {
+  const [volumeOn, setVolumeOn] = useState(false)
+
+  const handleClick = (e) => {
+    if (e.target.nodeName === "svg") {
+      if(item.title) {
+        if (volumeOn) {
+          e.target.previousElementSibling.previousElementSibling.muted = true;
+          setVolumeOn(false);
+        } else {
+          e.target.previousElementSibling.previousElementSibling.muted = false;
+          setVolumeOn(true);
+        }
+      }
+      if (volumeOn) {
+        e.target.previousElementSibling.muted = true;
+        setVolumeOn(false);
+      } else {
+        e.target.previousElementSibling.muted = false;
+        setVolumeOn(true);
+      }
+    } else {
+      if(item.title) {
+        if (volumeOn) {
+          e.target.parentElement.previousElementSibling.previousElementSibling.muted = true;
+          setVolumeOn(false);
+        } else {
+          e.target.parentElement.previousElementSibling.previousElementSibling.muted = false;
+          setVolumeOn(true);
+        }
+      }
+      if (volumeOn) {
+        e.target.parentElement.previousElementSibling.muted = true;
+        setVolumeOn(false);
+      } else {
+        e.target.parentElement.previousElementSibling.muted = false;
+        setVolumeOn(true);
+      }
+    }
+  }
+
   return (
     <div className={`horizontal-content ${item.tagname}`}>
-        <video src={item.content} autoPlay loop muted></video>
+        <video src={item.content} autoPlay muted loop></video>
         {
             item.title && 
 
@@ -13,7 +56,12 @@ export const HorizontalContent = ({item}) => {
                 <p>{item.copyright} {item.by}</p>
             </div>
         }
+        {
+          volumeOn ? 
+          <BsFillVolumeUpFill className="volume-on" onClick={handleClick} />
+          :
+          <BsFillVolumeMuteFill className="volume-muted" onClick={handleClick} />
+        }
     </div>
   )
 }
-
--- src/components/VerticalContent/VerticalContent.jsx
+++ src/components/VerticalContent/VerticalContent.jsx
@@ -1,6 +1,30 @@
 import "./VerticalContent.css";
+import { BsFillVolumeMuteFill } from "react-icons/bs";
+import { BsFillVolumeUpFill } from "react-icons/bs";
+import { useState } from "react";
 
 export const VerticalContent = ({item}) => {
+  const [volumeOn,setVolumeOn] = useState(false);
+
+  const handleClick = (e) => {
+    if (e.target.nodeName === "svg") {
+      if (volumeOn) {
+        e.target.previousElementSibling.muted = true;
+        setVolumeOn(false);
+      } else {
+        e.target.previousElementSibling.muted = false;
+        setVolumeOn(true);
+      }
+    } else {
+      if (volumeOn) {
+        e.target.parentElement.previousElementSibling.muted = true;
+        setVolumeOn(false);
+      } else {
+        e.target.parentElement.previousElementSibling.muted = false;
+        setVolumeOn(true);
+      }
+    }
+  }
 
   return (
     <div className={`vertical-content ${item.tagname}`}>
@@ -10,7 +34,15 @@ export const VerticalContent = ({item}) => {
         <p><span>&#62;</span>{item.copyright}</p>
         <p><span>&#62;</span>{item.by}</p>
       </div>
-      <video src={item.content} autoPlay loop muted></video>
+      <div>
+      <video src={item.content} autoPlay muted loop ></video>
+      {
+          volumeOn ? 
+          <BsFillVolumeUpFill className="volume-on" onClick={handleClick} />
+          :
+          <BsFillVolumeMuteFill className="volume-muted" onClick={handleClick} />
+      }
+      </div>
     </div>
   )
 }
