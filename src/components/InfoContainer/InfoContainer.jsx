3ff96dea4a738c07022034ecc8ab86b7ea42fd51
Ignacio Kruchowski <ignakruchowski@gmail.com>
2023-04-07 00:09:31 -0300

    Modificar estructura infoContainer

--- src/components/InfoContainer/InfoContainer.jsx
+++ src/components/InfoContainer/InfoContainer.jsx
@@ -1,5 +1,16 @@
 import './InfoContainer.css';
 
+const info = [
+    {
+        title: "SKILLS",
+        data: ["ADOBE AFTER EFFECTS", "ADOBE PHOTOSHOP", "ADOBE ILLUSTRATOR", "BLENDER"]
+    },
+    {
+        title: "CLIENTS",
+        data: ["PARAMOUNT", "BANCO GALICIA", "PEDIDOS YA", "MODO", "CODERHOUSE", "AND OTHERS..."]
+    }
+]; 
+
 export const InfoContainer = () => {
     return (
         <div className='info-container'>
@@ -11,27 +22,18 @@ export const InfoContainer = () => {
                 <h5><span>&#62;</span>HIS WORK SPANS MOTION GRAPHICS, VISUAL DESIGN AND 3D GENERALISM</h5>
             </div>
             <div className='info-content work'>
-                <div>
-                    <h5><span>&#62;</span>SKILLS:</h5>
-                    <ul>
-                        <li>ADOBE AFTER EFFECTS</li>
-                        <li>ADOBE PHOTOSHOP</li>
-                        <li>ADOBE ILLUSTRATOR</li>
-                        <li>BLENDER</li>
-                    </ul>
-                </div>
-                <div>
-                    <h5><span>&#62;</span>CLIENTS:</h5>
-                    <ul>
-                        <li>NICKELODEON</li>
-                        <li>COCA-COLA</li>
-                        <li>BANCO GALICIA</li>
-                        <li>PEDIDOS YA</li>
-                        <li>MODO</li>
-                        <li>CODERHOUSE</li>
-                        <li>AND OTHERS...</li>
-                    </ul>
-                </div>
+                {
+                    info.map((item,index) => {
+                        return(
+                            <div key={index}>
+                                <h5><span>&#62;</span>{item.title}:</h5>
+                                <ul>
+                                    {item.data.map((dataItem,index) => <li key={index}>{dataItem}</li>)}
+                                </ul>
+                            </div>
+                        );
+                    })
+                }
             </div>
             <p className='contact'><span>©2023</span>Drop him an email and work together! <span>agozski@gmail.com</span></p>
         </div>
